2ef2adfbba9763ba38fc0f1e8ca03dc2e5e5ec35
cena261 <khanhtu261@gmail.com>
2025-12-13 19:15:41 +0700

    feat: migration update

---
+++ ShoeStore/Migrations/202512131211170_UpdateProduct.cs
@@ -0,0 +1,18 @@
+﻿namespace ShoeStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateProduct : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Products", "CreatedAt", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "CreatedAt", c => c.DateTime(nullable: false));
+        }
+    }
+}
